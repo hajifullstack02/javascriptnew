5ef13318fa18f217936499f6e3647c6c5114e0dc
hajifullstack@02 <haji.fullstack.com>
2025-09-07 20:47:05 +0530

    anonymousfunctions

---
+++ anonymousfunction.js
@@ -0,0 +1,26 @@
+//anonymous function
+
+const topic = function(){
+ console.log("javaScript");
+}
+topic();
+
+// normal function 
+
+function companyName(){
+    console.log("Misard");
+}
+
+companyName();
+
+
+//return type function
+function sum(){
+  a=10;
+  b=25;
+ return a+b;
+}
+
+console.log(sum());
+
+
