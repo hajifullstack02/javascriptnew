1ec641a5925901e201de0a1657f91edad8fd912d
hajifullstack@02 <haji.fullstack.com>
2025-09-07 19:51:55 +0530

    variables code

---
+++ 1.practice.js
@@ -0,0 +1,38 @@
+//primative variables types
+
+//number
+let x= 10;   
+console.log(x);
+let pie=3.14;
+console.log(pie);
+
+//String
+let firstName="haji";
+let lastName ="bahadurshavali"
+console.log(firstName);
+console.log(lastName);
+
+
+let fullName=`my full name is - ${firstName} ${lastName}`;
+console.log(fullName);
+
+//boolean 
+
+ let isStudent=false;
+ let isEmployee= true;
+ console.log(isStudent);
+ console.log(isEmployee);
+
+ //undefind
+
+ let companyName;
+ console.log(companyName);
+
+
+ //null
+let salary=null;
+console.log(salary);
+
+ //symbol
+ let symb= Symbol("js");
+ console.log(symb);
