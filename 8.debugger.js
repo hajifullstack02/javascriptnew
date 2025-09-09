87a31cb0dc1cc5fda16cfef6b6f5f5f73415c7fe
hajifullstack@02 <haji.fullstack.com>
2025-09-09 07:06:56 +0530

    debugger

---
+++ 8.debugger.js
@@ -0,0 +1,27 @@
+//debugger use example
+
+function isCompany(companyName){
+    let emp1=companyName;
+    console.log(emp1);
+    
+}
+
+
+
+function experience(workexp){
+    let person1= workexp;
+    console.log("work experience :", person1);
+}
+
+
+isCompany("company Name : misard");
+debugger;
+
+
+experience(5);
+debugger
+
+console.log("code is completly executed ")
+
+
+
