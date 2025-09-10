4733c275fc8b370fde688cf16a9360a0a18cd000
hajifullstack@02 <haji.fullstack.com>
2025-09-10 08:38:29 +0530

    conditionalstatements

---
+++ 11.conditionalstatements.js
@@ -0,0 +1,54 @@
+// conditional statements
+// if  elseif  else .....
+
+if(true){
+    console.log("Welcome to 'if' statements")
+};
+
+
+let x=10;
+
+if(x>15){
+    console.log("x is greater than 15");
+}
+else{
+     console.log("x is lesser than 15");
+};
+
+
+let time =14;
+
+if(time <11){
+    console.log("Hi, Good morning");
+}
+else if(time <18){
+        console.log("Hi, Good afternoon");
+}
+else{
+     console.log("Hi, Good evening");
+}
+
+
+
+// switch case
+let day=4;
+
+switch(day){
+    case day=0: console.log( "sunday");
+    break;
+    case day=1: console.log( "monday");
+    break;
+    case day=2: console.log( "tuesday");
+    break;
+    case day=3: console.log( "wednesday");
+    break;
+    case day=4: console.log( "thursday");
+    break;
+    case day=5: console.log( "friday");
+    break;
+    case day=6: console.log( "saturday");
+    break;
+
+}
+
+
