a4773c98f4e9468fffe0bc2d981cf34750adbba1
hajifullstack@02 <haji.fullstack.com>
2025-09-17 22:33:15 +0530

    sync_call

---
+++ 20.sync_call.js
@@ -0,0 +1,25 @@
+function step1(){
+      console.log(" completed Step1");
+}
+function step2(){
+      console.log("completed Step2");
+}
+function step3(){
+      console.log("completed Step3");
+}
+function step4(){
+      console.log(" completed Step4");
+}
+function step5(){
+      console.log("completed Step5");
+}
+function step6(){
+      console.log("completed Step6");
+}
+
+step1();   
+step2();
+step3();
+step4();   
+step5();
+step6();
